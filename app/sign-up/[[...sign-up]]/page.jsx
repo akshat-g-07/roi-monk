d8aa14f602f5042feca1c6291f2c2517f14c71ba
Akshat Garg <akshatg805@gmail.com>
2024-05-04 22:40:45 +0530

    Finishing Touchup of Authentication

--- app/sign-up/[[...sign-up]]/page.jsx
+++ app/sign-up/[[...sign-up]]/page.jsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <>
-      <div className="h-screen w-screen flex items-center justify-center">
+      <div className="h-screen w-screen flex items-center justify-center dark bg-background">
         <SignUp path="/sign-up" />
       </div>
     </>
