3b43f3d8ba2839644adc369cf09a78f5d2e8556a
Akshat Garg <akshatg805@gmail.com>
2024-05-08 23:53:59 +0530

    Feedback Buttton Top Left Header

---
+++ components/FeedbackButton.jsx
@@ -0,0 +1,12 @@
+import AddCommentIcon from "@mui/icons-material/AddComment";
+
+export default function FeedbackButton() {
+  return (
+    <>
+      <div className="w-32 text-base font-normal bg-white text-black p-2 rounded-md hover:bg-white/90">
+        <AddCommentIcon className="mr-2" />
+        Feedback
+      </div>
+    </>
+  );
+}
