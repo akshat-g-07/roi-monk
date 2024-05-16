fe3c9d82fd6eb0ea8f7e46e15f9a3eb8780d8fe0
Akshat Garg <akshatg805@gmail.com>
2024-05-16 23:16:38 +0530

    dummy data-table columns

---
+++ lib/payment-cols.jsx
@@ -0,0 +1,74 @@
+"use client";
+
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export const columns = [
+  {
+    accessorKey: "status",
+    header: "Status",
+  },
+  {
+    accessorKey: "email",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Email
+          {column.getIsSorted() === "asc" ? (
+            <ArrowUpwardIcon sx={{ marginLeft: "4px", fontSize: "16px" }} />
+          ) : (
+            <ArrowDownwardIcon sx={{ marginLeft: "4px", fontSize: "16px" }} />
+          )}
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "amount",
+    header: "Amount",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const payment = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <MoreHorizIcon sx={{ fontSize: "20px" }} />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(payment.id)}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
+
+// for prod:
+// investments name, money invested, capital gains, roi, annualized roi, options
