6e2a73c3323a44048b13c48a107da62961bdb264
Akshat Garg <akshatg805@gmail.com>
2024-05-16 23:15:43 +0530

    Dummy DB for DataTable

---
+++ data/payments.js
@@ -0,0 +1,32 @@
+export const payments = [
+  {
+    id: "1",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  {
+    id: "2",
+    amount: 125,
+    status: "processing",
+    email: "a@gmail.com",
+  },
+  {
+    id: "3",
+    amount: 25,
+    status: "success",
+    email: "b@gmail.com",
+  },
+  {
+    id: "4",
+    amount: 65,
+    status: "failed",
+    email: "c@gmail.com",
+  },
+  {
+    id: "5",
+    amount: 145,
+    status: "processing",
+    email: "d@gmail.com",
+  },
+];
