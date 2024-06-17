21ca0e8375c248827b563755b40e1686f4c09857
Akshat Garg <akshatg805@gmail.com>
2024-06-18 00:02:33 +0530

    Search Bar BG & Text Color Fix

--- components/SearchOption.jsx
+++ components/SearchOption.jsx
@@ -50,6 +50,10 @@ export default function SearchOption() {
             color: "rgba(255,255,255,1)",
           },
         }}
+        PaperComponent={PaperComp}
+        renderOption={(props, option) => (
+          <CustomListItem props={props} option={option} />
+        )}
         autoHighlight
         options={top100Films}
         getOptionLabel={(option) => option.label}
@@ -67,3 +71,34 @@ export default function SearchOption() {
     </div>
   );
 }
+
+const CustomListItem = ({ props, option }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <li
+      {...props}
+      style={{
+        backgroundColor: isHovered ? "black" : "inherit",
+        color: "inherit",
+      }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      {option.label}
+    </li>
+  );
+};
+
+const PaperComp = (props) => {
+  console.log(props);
+  return (
+    <div
+      style={{
+        backgroundColor: "rgba(14, 28, 64,1)",
+        color: "hsl(210 40% 98%)",
+      }}
+      {...props}
+    />
+  );
+};
