aa96eaec9a62f078164fca2fc4d279035fb47c70
Akshat Garg <akshatg805@gmail.com>
2024-07-27 23:55:56 +0530

    feat: updating data table inline with portfolio page

--- lib/payment-cols.jsx
+++ lib/payment-cols.jsx
@@ -1,19 +1,17 @@
 "use client";
 
+import { format } from "date-fns";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
-import ArrowOutwardIcon from "@mui/icons-material/ArrowOutward";
+import EditIcon from "@mui/icons-material/Edit";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
-
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Checkbox } from "@/components/ui/checkbox";
@@ -42,18 +40,18 @@ export const columns = [
     enableHiding: false,
   },
   {
-    accessorKey: "status",
-    header: "Status",
+    accessorKey: "transactionName",
+    header: "Name",
   },
   {
-    accessorKey: "email",
+    accessorKey: "type",
     header: ({ column }) => {
       return (
         <Button
           variant="ghost"
           onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
         >
-          Email
+          Type
           {column.getIsSorted() && (
             <>
               {column.getIsSorted() === "asc" ? (
@@ -68,16 +66,111 @@ export const columns = [
         </Button>
       );
     },
+    cell: ({ row }) => {
+      return (
+        <div className="flex items-center">
+          <ArrowDownwardIcon
+            sx={{
+              rotate: `${row.getValue("type") === "CR" ? "180deg" : "0deg"}`,
+              color: `${row.getValue("type") === "CR" ? "green" : "red"}`,
+              fontSize: "14px",
+              marginRight: "5px",
+            }}
+          />
+          <p>{row.getValue("type") === "CR" ? "Credit" : "Debit"}</p>
+        </div>
+      );
+    },
   },
   {
     accessorKey: "amount",
-    header: "Amount",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Amount
+          {column.getIsSorted() && (
+            <>
+              {column.getIsSorted() === "asc" ? (
+                <ArrowUpwardIcon sx={{ marginLeft: "4px", fontSize: "16px" }} />
+              ) : (
+                <ArrowDownwardIcon
+                  sx={{ marginLeft: "4px", fontSize: "16px" }}
+                />
+              )}
+            </>
+          )}
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return (
+        <div
+          style={{
+            paddingLeft: "17.5px",
+          }}
+        >
+          $ {row.getValue("amount")}
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "transactionDate",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Transaction Date
+          {column.getIsSorted() && (
+            <>
+              {column.getIsSorted() === "asc" ? (
+                <ArrowUpwardIcon sx={{ marginLeft: "4px", fontSize: "16px" }} />
+              ) : (
+                <ArrowDownwardIcon
+                  sx={{ marginLeft: "4px", fontSize: "16px" }}
+                />
+              )}
+            </>
+          )}
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return (
+        <div
+          style={{
+            paddingLeft: "20px",
+          }}
+        >
+          {format(row.getValue("transactionDate"), "PPP")}
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "comments",
+    header: "Comments",
+    cell: ({ row }) => {
+      return (
+        <div
+          style={{
+            maxWidth: "500px",
+          }}
+        >
+          {row.getValue("comments").length > 0 ? row.getValue("comments") : "-"}
+        </div>
+      );
+    },
   },
   {
     id: "actions",
-    cell: ({ row }) => {
-      const payment = row.original;
-
+    header: "Actions",
+    cell: () => {
       return (
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
@@ -86,11 +179,9 @@ export const columns = [
             </Button>
           </DropdownMenuTrigger>
           <DropdownMenuContent align="end" className="dark">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuSeparator />
             <DropdownMenuItem>
-              <ArrowOutwardIcon className="mr-2 size-3.5" />
-              Open in a new tab
+              <EditIcon className="mr-2 size-3.5" />
+              Edit
             </DropdownMenuItem>
             <DropdownMenuItem>
               <ContentCopyIcon className="mr-2 size-3.5" />
@@ -106,6 +197,3 @@ export const columns = [
     },
   },
 ];
-
-// for prod:
-// investments name, money invested, capital gains, roi, annualized roi, options
