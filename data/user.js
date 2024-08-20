635b1d9abde91905add38e12071f8a932a807edd
Akshat Garg <akshatg805@gmail.com>
2024-08-20 23:04:48 +0530

    fix: no user edge case

--- data/user.js
+++ data/user.js
@@ -2,6 +2,7 @@ import { currentUser } from "@clerk/nextjs/server";
 
 export async function getUserEmail() {
   const user = await currentUser();
+  if (!user) return null;
   const userEmail = user.emailAddresses[0].emailAddress;
   return userEmail;
 }
