c84514cf3d4cce431174121f5477698b920c4ecc
Akshat Garg <akshatg805@gmail.com>
2024-10-15 13:32:06 +0530

    feat: extending for custom classnames

--- components/common/loading.jsx
+++ components/common/loading.jsx
@@ -1,12 +1,20 @@
+import { cn } from "@/lib/utils";
 import { CircularProgress } from "@mui/material";
 
-export default function Loading({ size = "3rem", absolute = false }) {
+export default function Loading({
+  className,
+  size = "3rem",
+  absolute = false,
+}) {
   return (
     <>
       <div
-        className={`size-full z-10 bg-primary/40 flex items-center justify-center ${
-          absolute && "absolute z-10"
-        }`}
+        className={cn(
+          `size-full z-10 bg-primary/40 flex items-center justify-center ${
+            absolute && "absolute z-10"
+          }`,
+          className
+        )}
       >
         <CircularProgress size={size} />
       </div>
