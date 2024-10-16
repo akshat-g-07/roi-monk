708e0aa54f8f6edaba9f0ff747a643c18dca30b2
Akshat Garg <akshatg805@gmail.com>
2024-10-16 12:03:39 +0530

    fix: lil fix

--- components/common/loading.jsx
+++ components/common/loading.jsx
@@ -1,18 +1,12 @@
 import { cn } from "@/lib/utils";
 import { CircularProgress } from "@mui/material";
 
-export default function Loading({
-  className,
-  size = "3rem",
-  absolute = false,
-}) {
+export default function Loading({ className, size = "3rem" }) {
   return (
     <>
       <div
         className={cn(
-          `size-full bg-primary/40 flex items-center justify-center ${
-            absolute && "absolute z-10"
-          }`,
+          "size-full bg-primary/40 flex items-center justify-center",
           className
         )}
       >
