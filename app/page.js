450854fd6b6d4c1a5086e932c70e2fda83b8318a
Akshat Garg <akshatg805@gmail.com>
2025-03-26 23:45:43 +0530

    feat: landing page

--- app/page.js
+++ app/page.js
@@ -1,3 +1,16 @@
+import Features from "@/components/landing-page/features";
+import HeroSection from "@/components/landing-page/hero-section";
+import NavBar from "@/components/landing-page/navbar";
+
 export default function Page() {
-  return <>This is landing page</>;
+  return (
+    <>
+      <div className="w-full h-full max-w-7xl">
+        <NavBar />
+        <HeroSection />
+        <Features />
+        pricing faq policies
+      </div>
+    </>
+  );
 }
