9afc3843f8256f4abf71611296af0be8025216ea
Akshat Garg <akshatg805@gmail.com>
2025-04-04 23:42:56 +0530

    fix: check if portfolio exists

--- app/(protected)/portfolio/[portfolioName]/page.jsx
+++ app/(protected)/portfolio/[portfolioName]/page.jsx
@@ -5,9 +5,23 @@ import { useServerAction } from "@/hooks/useServerAction";
 import Portfolio from "@/components/portfolio/portfolio";
 import Loading from "@/components/common/loading";
 import Error from "@/components/common/error";
+import { GetPortfolioByName } from "@/actions/portfolio";
 
 export default function Page({ params }) {
   const { portfolioName } = params;
+  const { data: portfolio } = useServerAction(
+    GetPortfolioByName,
+    decodeURI(portfolioName)
+  );
+
+  if (!portfolio)
+    return (
+      <div className="grid size-full items-center justify-center">
+        <p className="w-fit">
+          Portfolio with {decodeURI(portfolioName)} name doesn't exist.
+        </p>
+      </div>
+    );
   const {
     isLoading,
     data: originalTransactions,
